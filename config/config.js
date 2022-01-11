d1eb659fed8e8c587a5b33fa50502fb0eb7d7d54
Kaushalendra Singh <Kaushalendra.Singh@Maplelabs.Com>
2022-01-11 23:09:12 +0530

    data base configuration added

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  uri: "mongodb+srv://kaushal:N@rayan123@cluster0.5qlzp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  uri: "mongodb+srv://kaushal:N@rayan123@cluster0.5qlzp.mongodb.net/medical?retryWrites=true&w=majority",
   uri1: "mongodb+srv://admin:XSpqVfxMDI6O2Ne8@medical.cwl60.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   localuri: "mongodb://localhost:27017",
   dbName: "medical",
